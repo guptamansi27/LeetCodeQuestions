a9c6b6468efe78d59528d67285bc877515b24d2f
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-03-29 23:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (91.30%) - LeetSync

--- 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minCostToMoveChips(int[] position) {
+        /* Method 1: O(n^2)
         int minCost = Integer.MAX_VALUE;
         int l=position.length;
         for(int i=0;i<l;i++){
@@ -11,6 +12,13 @@ class Solution {
             }
             minCost=Math.min(c,minCost);
         }
-        return minCost;
+        return minCost;*/
+    //Method 2 optimal O(n)
+    int even=0,odd=0;
+        for(int ele:position){
+            if(ele%2==0) even++;
+            else odd++;
+        }
+        return Math.min(even,odd);
     }
 }
