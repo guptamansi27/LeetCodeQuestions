640f37cc58913ee8ffef498938fdccbe3a5b185f
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-04-22 18:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (87.48%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean canWinNim(int n) {
+        /* Method 1
         if(n%4==0)
             return false;
         else
-            return true;
+            return true;*/
+        return n%4!=0;
     }
 }
