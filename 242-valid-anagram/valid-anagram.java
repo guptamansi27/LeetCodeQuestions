7878f9dd9ad7a95f983b7830a52e670f6f5c2702
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-04-29 19:11:37 +0530

    Time: 5 ms (61.86%) | Memory: 44.3 MB (74.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
          if (s.length() != t.length()) return false;
+        /* Method 1: T.C->O(nlogn)  S.C->O(n) 
+        This is not optimal dur to sorting
 
         char a[]=s.toCharArray();
         char b[]=t.toCharArray();
@@ -8,7 +10,22 @@ class Solution {
         Arrays.sort(a);
         Arrays.sort(b);
 
-        return Arrays.equals(a,b);
-            
+        return Arrays.equals(a,b);*/
+        //Method 2: Optimal  T.C->O(n)  S.C->O(1)
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;  // increment for s
+            count[t.charAt(i) - 'a']--;  // decrement for t
+        }
+
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+
+        return true;
     }
 }
+            
+  
