a58dee9e92e0f8e42e1f8f39d7c06073d59bd748
Mansi Gupta <150320404+guptamansi27@users.noreply.github.com>
2026-04-30 21:21:53 +0530

    Time: 6 ms (89.41%) | Memory: 43.7 MB (85.86%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        /*Method 1
         if (s.length() != t.length()) return false;
 
         HashMap<Integer, Integer> mapST = new HashMap<>();
@@ -24,6 +25,24 @@ class Solution {
             } else {
                 mapTS.put(c2, c1);
             }
+        }
+        return true;*/
+        //Method 2
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            // if last seen positions differ → not isomorphic
+            if (mapS[c1] != mapT[c2]) {
+                return false;
+            }
+
+            // store index + 1
+            mapS[c1] = i + 1;
+            mapT[c2] = i + 1;
         }
 
         return true;
